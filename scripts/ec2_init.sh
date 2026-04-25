dd40141ed7e0be04723a6ede103649b0e1794ee1
Mohammad Alzoubi <alzoubim@moravian.edu>
2026-04-25 14:39:40 -0400

    fixed ec2 script

--- scripts/ec2_init.sh
+++ scripts/ec2_init.sh
@@ -2,9 +2,9 @@ echo "Initializing EC2 instance for Discord bot deployment"
 echo "Installing dependencies: Java 21, Git, Redis 6"
 yum install maven-amazon-corretto21 git redis6 -y
 echo "Cloning Discord bot project repository"
-git clone https://github.com/cs220s26/Discord-bot-project-group-6.git /Discord-bot-project-group-6
+git clone https://github.com/cs220s26/Discord-bot-project-group-6.git /home/ec2-user/Discord-bot-project-group-6
 echo "Packaging the bot application with Maven"
-cd /Discord-bot-project-group-6
+cd /home/ec2-user/Discord-bot-project-group-6
 mvn package
 echo "Starting Redis server"
 systemctl enable redis6
@@ -12,8 +12,8 @@ systemctl start redis6
 echo "Putting service file in systemd"
 cp deploy/typingracebot.service /etc/systemd/system/
 systemctl daemon-reload
-systemctl enable typingracebot
-systemctl start typingracebot
+sudo systemctl enable typingracebot
+sudo systemctl start typingracebot
 echo "Checking service status"
-systemctl status typingracebot
+sudo systemctl status typingracebot
 journalctl -u typingracebot.service -f
